d3d47c12a0f45c3ed921f64be6e8244ee1d6d0b8
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-21 19:36:00 +0100

    test_snackbar

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -10,9 +10,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp>{
+  String _text ='';
+
+  final GlobalKey<ScaffoldState> _scaffoldState = new GlobalKey<ScaffoldState>();
+
+  void _onChanged(String value){
+    setState(){
+     _text=value;
+    }
+  }
+
+  void _showSnackBar(String value){
+     if(value.isEmpty) return ;
+     _scaffoldState.currentState.showSnackBar(new SnackBar(
+       content: new Text(value),
+     ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      key: _scaffoldState,
       title: 'Login Demo',
       home: new Scaffold(
         appBar: new AppBar(
@@ -21,6 +39,14 @@ class _MyAppState extends State<MyApp>{
             new IconButton(icon:new Icon(Icons.home),onPressed: null,),
             new IconButton(icon: new Icon(Icons.exit_to_app),onPressed: null,)
           ],
+        ),
+        body: new Container(
+          child: new Center(
+            child:new Column(children: <Widget>[
+              new TextField(onChanged: (String value){_onChanged(value);},),
+              new RaisedButton(child:new Text('Snack'),onPressed: (){_showSnackBar(_text);},)
+            ],) 
+          ,),
         ),
       ),
     );
