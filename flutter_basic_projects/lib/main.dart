d8009479b81b54172485b764529c945385fdf201
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-21 20:17:33 +0100

    alert_test

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -10,8 +10,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp>{
+  
+  
   @override
   Widget build(BuildContext context) {
+    String _text;
+    void _onChange(String value){
+    setState(){
+      _text=value;
+    }
+  }
+  void _showAlert(String string){
+    print(_text);
+  /*  AlertDialog alertDialog = new AlertDialog(
+      content: new Text(string,style: new TextStyle(fontSize: 30.0 ),),
+      actions: <Widget>[
+
+      ],
+    );
+    showDialog(context: context,child: alertDialog); */
+  }
     return new MaterialApp(
       title: 'Login Demo',
       home: new Scaffold(
@@ -22,6 +40,14 @@ class _MyAppState extends State<MyApp>{
             new IconButton(icon: new Icon(Icons.exit_to_app),onPressed: null,)
           ],
         ),
+        body : new Container(child :
+          new Column(
+              children: <Widget>[
+                new TextField(onChanged: (String value){_onChange(value);}),
+                new RaisedButton(child:new Text('Alert'),onPressed: (){_showAlert(_text);},),
+              ],
+          )
+        )
       ),
     );
   }
