2989145871f41d4a16575534ccfe12bc01a47e8f
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-21 12:17:14 +0100

    basic_requirements
    things like the utility of  key and a textEdittingController , and the  callBack

--- flutter_basic_projects/lib/login.dart
+++ flutter_basic_projects/lib/login.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
+class login extends StatelessWidget {
+   const login ({
+        Key key, // Key helps identify the actuel component 
+        @required this.onSubmit,
+      }) :super(key:key);
+
+    final VoidCallback onSubmit ;  // it will allow other peaple to use this component with a callback
+
+    static final TextEditingController _user  = new TextEditingController(); // it will allow us to work with a text field
+
+    static final TextEditingController _pass  = new TextEditingController();
+
+@override
+  Widget build(BuildContext context) {
+
+    return null;
+  }
+}
