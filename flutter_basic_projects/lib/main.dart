b33a3a0b177c1cc16ac3eb8d48a946a25b991de9
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-23 18:28:58 +0100

    Update main.dart

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp>{
       title: 'Login Demo',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Login Page'),
+          title: new Text('TextField Page'),
           actions: <Widget>[
             new IconButton(icon:new Icon(Icons.home),onPressed: null,),
             new IconButton(icon: new Icon(Icons.exit_to_app),onPressed: null,)
