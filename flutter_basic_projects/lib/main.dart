d6cdd7461e1568ab7226fc0dca5a95192d0779bd
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-26 04:56:51 +0100

    Update main.dart

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+
+
 void main() => runApp(new MaterialApp(home:
   MyApp()
   ));
@@ -11,10 +13,29 @@ class MyApp extends StatefulWidget {
     return new _MyAppState();
   }
 }
-
+enum Answer{YES,NO,MAYBE}
 class _MyAppState extends State<MyApp>{
   String answer ='';
-  
+   
+  void _setAnswer(String value){
+    setState(() {
+     answer=value; 
+    });
+  } 
+
+  Future<Null> _askuser() async {
+    switch(){
+      case Answer.YES:
+           _setAnswer('yes');
+           break;
+      case Answer.NO:
+           _setAnswer('no');
+           break;
+      case Answer.MAYBE:
+           _setAnswer('maybe');
+           break;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
