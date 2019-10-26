1b0302def9c43014da15186c6c0ae0c93e483f90
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-26 04:09:27 +0100

    Update main.dart

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
