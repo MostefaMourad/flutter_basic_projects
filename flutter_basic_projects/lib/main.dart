5918ec94891ef550bb9a4d833268c2a7f3dac697
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-26 04:27:32 +0100

    Update main.dart

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -12,12 +12,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
+  DateTime _date = new DateTime.now();
+  DateTime date = new Ti
+  TimeOfDay _time = new TimeOfDay.now();
+
+  Future<Null> _selectDate (BuildContext context) async {
+     final DateTime picked = await showDatePicker(
+       context: context,
+       initialDate: _date,
+       firstDate: new DateTime(2016),
+       lastDate: new DateTime(2019)
+     );
+
+     if(picked != null && picked != _date){
+       setState((){
+         _date=picked;
+       });
+     }
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text('Demo Page'),
+        ),
+        body: new Container(
+          child: new Column(children: <Widget>[
+            new RaisedButton(child: new Text('Cklick me dude !'),onPressed: (){_selectDate(context);},)
+          ],),
         ),
     );
   }
