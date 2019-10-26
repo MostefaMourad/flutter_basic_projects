c382d563880a995776d9e416b50843240c2cddca
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-26 02:55:47 +0100

    MaterialApp
    when popoing and the error says
    (Exception: "Another exception was thrown: Navigator operation requested with a context that does not include a Navigator. ")
    that means instead of returning a a Marerial app you should return a scoffold
    There is some issue with MateriaApp context in the library.The Navigator won't work if you're doing in under MateriaApp context like this

--- flutter_basic_projects/lib/main.dart
+++ flutter_basic_projects/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
-  home:MyApp())
+  home:new MyApp())
 );
 
 class MyApp extends StatefulWidget {
@@ -15,23 +15,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Login Demo',
-      home: new Scaffold(
+    return new Scaffold(
         appBar: new AppBar(
           title: new Text('Live Template Page'),
         ),
         body: new Container(child: null,),
-        drawer: new Drawer(
-          child: new Container(
-           padding: new EdgeInsets.all(32.0),
-           child : new Column(children: <Widget>[
-             new Text('Click to close'),
-             new RaisedButton(onPressed:(){Navigator.pop(context);},child: new Text('Close'),)
-           ],)
-          ),
-        ),
-      ),
     );
   }
 }
